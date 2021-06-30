13e1c6de85cfaf12488f38fb985196e77a1ae057
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-30 19:52:57 -0300

    Add date on info product page

--- components/Game/InfoGame/InfoGame.tsx
+++ components/Game/InfoGame/InfoGame.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React from 'react';
 import ReactPlayer from 'react-player/lazy';
 import { GameList } from '../../../interfaces/gamesInterfaces';
@@ -12,6 +13,13 @@ const InfoGame = ({ game }: InfoGame) => {
     <div className="info-game">
       <ReactPlayer className="info-game__video" url={game.video} controls />
       <CarouselScreenshots title={game.title} screenshots={game.screenshots} />
+      <div className="info-game__content">
+        <div dangerouslySetInnerHTML={{ __html: game.summary }} />
+        <div className="info-game__content-date">
+          <h4>Release Date:</h4>
+          <p>{moment(game.releaseDate).format('LL')}</p>
+        </div>
+      </div>
     </div>
   );
 };
